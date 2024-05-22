e65328b5f7519d225321ad439a14f6b16605942e
Navneet-Shukla-123 <Navneet.shukla@pantelwar.com>
2024-05-22 11:04:27 +0530

    Added comments

--- models/models.go
+++ models/models.go
@@ -2,17 +2,20 @@ package models
 
 import "time"
 
+// User model for Email and Password for user
 type User struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
+// GetResponse model will hold response from the url
 type GetResponse struct {
 	LastUpdateID int64      `json:"lastUpdateId"`
 	Bids         [][]string `json:"bids"`
 	Asks         [][]string `json:"asks"`
 }
 
+// Tokendata model for the token of the user
 type Tokendata struct {
 	Email      string    `json:"email"`
 	Token      string    `json:"token"`
